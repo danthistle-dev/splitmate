207859814f00c4aab5ebde75177161646a82fe73
Dan Thistlethwaite <thedanthistlethwaite@gmail.com>
2020-05-05 18:17:08 +1000

    Added split section to trip page

--- components/Trip.js
+++ components/Trip.js
@@ -1,28 +1,60 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Divider, Title, List } from 'react-native-paper';
+import { StyleSheet, View, ScrollView, SafeAreaView } from 'react-native';
+import { Divider, Title, List, Text } from 'react-native-paper';
 import Header from './Header';
-
-import data from '../test.json';
 
 const Trip = ({ route, navigation }) => {
   return(
     <View>
       <Header title={route.params.name} back nav={navigation} />
-      <Title style={styles.title}>Expenses</Title>
-      <Divider />
-      <List.Section>
-        {route.params.members.map((member, i) => {
-          return(
-            <List.Accordion title={member} key={i}>
-              <List.Item 
-                title={route.params.expenses[0].name} 
-                left={props => <List.Icon {...props} icon="subdirectory-arrow-right" />} 
-              />
-            </List.Accordion>
-          );
-        })}
-      </List.Section>
+      <SafeAreaView>
+        <ScrollView style={{ height: '85%' }}>
+          <Title style={styles.title}>Expenses</Title>
+          <Divider />
+          <List.Section>
+            {route.params.members.map((member, i) => {
+              return(
+                <List.Accordion title={member} key={i}>
+                  {route.params.expenses
+                    .filter(exp => exp.payer === member)
+                    .map((exp, i) => {
+                      return <List.Item
+                        key={i}
+                        style={styles.listItem}
+                        title={exp.name} 
+                        left={props => <List.Icon {...props} icon="subdirectory-arrow-right" />}
+                        right={() => <Text style={{ alignSelf: 'center', paddingRight: '10%' }}>${exp.cost}</Text>}
+                      />
+                    })}
+                </List.Accordion>
+              );
+            })}
+          </List.Section>
+
+          <Title style={styles.title}>Splits</Title>
+          <Divider />
+          <List.Section>
+            {route.params.members.map((member, i) => {
+              return(
+                <List.Accordion title={member} key={i}>
+                  {route.params.splits
+                    .filter(split => split.member === member)
+                    .map((split, i) => split.owes
+                    .map((owe, i) => {
+                      return <List.Item
+                        key={i}
+                        style={styles.listItem}
+                        title={owe.member} 
+                        left={props => <List.Icon {...props} icon="subdirectory-arrow-right" />}
+                        right={() => <Text style={{ alignSelf: 'center', paddingRight: '10%' }}>${owe.amount}</Text>}
+                      />
+                    }))}
+                </List.Accordion>
+              );
+            })}
+          </List.Section>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
@@ -34,6 +66,10 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 10
   },
+  listItem: {
+    backgroundColor: 'lightgrey',
+    display: 'flex'
+  }
 });
 
 export default Trip;
